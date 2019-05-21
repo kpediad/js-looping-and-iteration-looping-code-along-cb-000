56d2fdae011dd4ea9a1436aa8ed61f0ea5e95a0b
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 09:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function printBadges(employees) {
   }
   return employees;
 }
+
+function tailsNeverFails() {
+  coinFlip = Math.random().round();
+  // Tails == 1 and Heads == 0
+  let tailsCounter = 0;
+  while (coinflip != 0) {
+    tailsCounter++;
+    coinFlip = Math.random().round();
+  }
+  console.log(`You got ${tailsCounter} tails in a row!`);
+  return tailsCounter;
+}
