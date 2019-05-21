d9871f06f768cfe1a7a5fd4cd81a9bc340886a93
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 09:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function printBadges(employees) {
 }
 
 function tailsNeverFails() {
-  coinFlip = Math.random().round();
+  coinFlip = Math.round(Math.random());
   // Tails == 1 and Heads == 0
   let tailsCounter = 0;
   while (coinflip != 0) {
